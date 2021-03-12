7c0fe5abc7f6b5dc9c9f72c9d74408eedc2e04ed
Siarhei Berdachuk <berdachuk@gmail.com>
2021-03-12 20:17:20 +0300

    add DepartmentDtoService Implementation

---
+++ dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDtoDaoJdbc.java
@@ -0,0 +1,41 @@
+package com.epam.brest.dao.jdbc;
+
+import com.epam.brest.dao.DepartmentDtoDao;
+import com.epam.brest.model.dto.DepartmentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ *  Department DTO DAO implementation.
+ */
+@Component
+public class DepartmentDtoDaoJdbc implements DepartmentDtoDao {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentDtoDaoJdbc.class);
+
+    @Value("${departmentDto.findAllWithAvgSalary}")
+    private String findAllWithAvgSalarySql;
+
+
+    public DepartmentDtoDaoJdbc(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<DepartmentDto> findAllWithAvgSalary() {
+
+        LOGGER.debug("findAllWithAvgSalary()");
+        List<DepartmentDto> departments = namedParameterJdbcTemplate.query(findAllWithAvgSalarySql,
+                BeanPropertyRowMapper.newInstance(DepartmentDto.class));
+        return departments;
+    }
+
+}
