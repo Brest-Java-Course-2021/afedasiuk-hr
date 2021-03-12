a508d448cbb82769cdf664c2041b40f6d57308d3
Siarhei Berdachuk <berdachuk@gmail.com>
2021-03-12 22:07:12 +0300

    pass data model to UI

--- web-app/src/main/java/com/epam/brest/service/web_app/DepartmentController.java
+++ web-app/src/main/java/com/epam/brest/service/web_app/DepartmentController.java
@@ -1,5 +1,9 @@
 package com.epam.brest.service.web_app;
 
+import com.epam.brest.service.DepartmentDtoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class DepartmentController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    private final DepartmentDtoService departmentDtoService;
+
+    @Autowired
+    public DepartmentController(DepartmentDtoService departmentDtoService) {
+        this.departmentDtoService = departmentDtoService;
+    }
+
     /**
      * Goto departments list page.
      *
@@ -15,6 +28,8 @@ public class DepartmentController {
      */
     @GetMapping(value = "/departments")
     public final String departments(Model model) {
+        LOGGER.debug("departments()");
+        model.addAttribute("departments", departmentDtoService.findAllWithAvgSalary());
         return "departments";
     }
 
