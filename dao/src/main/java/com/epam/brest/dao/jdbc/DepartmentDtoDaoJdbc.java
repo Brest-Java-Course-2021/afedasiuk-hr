4a8239b1f98b9617ba381c97ed1844f9b3265a32
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2021-03-19 21:17:54 +0300

    spring-boot

--- dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDtoDaoJdbc.java
+++ dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDtoDaoJdbc.java
@@ -24,7 +24,6 @@ public class DepartmentDtoDaoJdbc implements DepartmentDtoDao {
     @Value("${departmentDto.findAllWithAvgSalary}")
     private String findAllWithAvgSalarySql;
 
-
     public DepartmentDtoDaoJdbc(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
