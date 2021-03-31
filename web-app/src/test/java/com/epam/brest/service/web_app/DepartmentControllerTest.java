2e56e724f6857855e32d5ab366dea007f1b334cb
viktar_zhyhunou <viktar_zhyhunou@epam.com>
2021-03-31 21:54:05 +0300

    web-app testing

--- web-app/src/test/java/com/epam/brest/service/web_app/DepartmentControllerTest.java
+++ web-app/src/test/java/com/epam/brest/service/web_app/DepartmentControllerTest.java
@@ -2,28 +2,24 @@ package com.epam.brest.service.web_app;
 
 import com.epam.brest.model.Department;
 import com.epam.brest.model.dto.DepartmentDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.epam.brest.service.DepartmentDtoService;
+import com.epam.brest.service.DepartmentService;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.client.ExpectedCount;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.math.BigDecimal;
-import java.net.URI;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static com.epam.brest.model.constants.DepartmentConstants.DEPARTMENT_NAME_SIZE;
 import static org.hamcrest.Matchers.allOf;
@@ -32,49 +28,37 @@ import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest
-class DepartmentControllerIT {
-
-    private static final String DEPARTMENT_DTOS_URL = "http://localhost:8088/department-dtos";
-    private static final String DEPARTMENTS_URL = "http://localhost:8088/departments";
+@WebMvcTest(DepartmentController.class)
+class DepartmentControllerTest {
 
     @Autowired
-    private WebApplicationContext wac;
-
     private MockMvc mockMvc;
 
-    @Autowired
-    RestTemplate restTemplate;
+    @MockBean
+    private DepartmentService departmentService;
 
-    private MockRestServiceServer mockServer;
+    @MockBean
+    private DepartmentDtoService departmentDtoService;
 
-    private ObjectMapper mapper = new ObjectMapper();
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-        mockServer = MockRestServiceServer.createServer(restTemplate);
-    }
+    @Captor
+    private ArgumentCaptor<Department> captor;
 
     @Test
     public void shouldReturnDepartmentsPage() throws Exception {
         DepartmentDto d1 = createDepartmentDto(1, "IT", BigDecimal.valueOf(150));
         DepartmentDto d2 = createDepartmentDto(2, "SECURITY", BigDecimal.valueOf(400));
         DepartmentDto d3 = createDepartmentDto(3, "MANAGEMENT", null);
-        mockServer.expect(ExpectedCount.once(), requestTo(new URI(DEPARTMENT_DTOS_URL)))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withStatus(HttpStatus.OK)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(mapper.writeValueAsString(Arrays.asList(d1, d2, d3)))
-                );
+        when(departmentDtoService.findAllWithAvgSalary()).thenReturn(Arrays.asList(d1, d2, d3));
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/departments")
         ).andDo(MockMvcResultHandlers.print())
@@ -103,7 +87,6 @@ class DepartmentControllerIT {
                         )
                 )))
         ;
-        mockServer.verify();
     }
 
     @Test
@@ -116,41 +99,32 @@ class DepartmentControllerIT {
                 .andExpect(view().name("department"))
                 .andExpect(model().attribute("isNew", is(true)))
                 .andExpect(model().attribute("department", isA(Department.class)));
-        mockServer.verify();
+        verifyNoMoreInteractions(departmentDtoService, departmentService);
     }
 
     @Test
     public void shouldAddNewDepartment() throws Exception {
-
-        mockServer.expect(ExpectedCount.once(), requestTo(new URI(DEPARTMENTS_URL)))
-                .andExpect(method(HttpMethod.POST))
-                .andRespond(withStatus(HttpStatus.OK)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body("1")
-                );
-
+        String testName = RandomStringUtils.randomAlphabetic(DEPARTMENT_NAME_SIZE);
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/department")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("departmentName", "test")
+                        .param("departmentName", testName)
         ).andExpect(status().isFound())
                 .andExpect(view().name("redirect:/departments"))
                 .andExpect(redirectedUrl("/departments"));
 
-        mockServer.verify();
+        verify(departmentService).create(captor.capture());
+
+        Department d = captor.getValue();
+        assertEquals(testName, d.getDepartmentName());
     }
 
     @Test
     public void shouldOpenEditDepartmentPageById() throws Exception {
         Department d = createDepartment(1, "IT");
-        mockServer.expect(ExpectedCount.once(), requestTo(new URI(DEPARTMENTS_URL + "/" + d.getDepartmentId())))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withStatus(HttpStatus.OK)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(mapper.writeValueAsString(d))
-                );
+        when(departmentService.findById(any())).thenReturn(Optional.of(d));
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/department/1")
+                MockMvcRequestBuilders.get("/department/" + d.getDepartmentId())
         ).andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
@@ -158,33 +132,22 @@ class DepartmentControllerIT {
                 .andExpect(model().attribute("isNew", is(false)))
                 .andExpect(model().attribute("department", hasProperty("departmentId", is(d.getDepartmentId()))))
                 .andExpect(model().attribute("department", hasProperty("departmentName", is(d.getDepartmentName()))));
-        mockServer.verify();
     }
 
     @Test
     public void shouldReturnToDepartmentsPageIfDepartmentNotFoundById() throws Exception {
         int id = 99999;
-        mockServer.expect(ExpectedCount.once(), requestTo(new URI(DEPARTMENTS_URL + "/" + id)))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withStatus(HttpStatus.OK)
-                        .contentType(MediaType.APPLICATION_JSON)
-                );
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/department/" + id)
         ).andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isFound())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("departments"));
-        mockServer.verify();
+        verify(departmentService).findById(id);
     }
 
     @Test
     public void shouldUpdateDepartmentAfterEdit() throws Exception {
-        mockServer.expect(ExpectedCount.once(), requestTo(new URI(DEPARTMENTS_URL)))
-                .andExpect(method(HttpMethod.PUT))
-                .andRespond(withStatus(HttpStatus.OK)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body("1")
-                );
+
         String testName = RandomStringUtils.randomAlphabetic(DEPARTMENT_NAME_SIZE);
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/department/1")
@@ -195,25 +158,22 @@ class DepartmentControllerIT {
                 .andExpect(view().name("redirect:/departments"))
                 .andExpect(redirectedUrl("/departments"));
 
-        mockServer.verify();
+        verify(departmentService).update(captor.capture());
+
+        Department d = captor.getValue();
+        assertEquals(testName, d.getDepartmentName());
     }
 
     @Test
     public void shouldDeleteDepartment() throws Exception {
-        int id = 3;
-        mockServer.expect(ExpectedCount.once(), requestTo(new URI(DEPARTMENTS_URL + "/" + id)))
-                .andExpect(method(HttpMethod.DELETE))
-                .andRespond(withStatus(HttpStatus.OK)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body("1")
-                );
+
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/department/3/delete")
         ).andExpect(status().isFound())
                 .andExpect(view().name("redirect:/departments"))
                 .andExpect(redirectedUrl("/departments"));
 
-        mockServer.verify();
+        verify(departmentService).delete(3);
     }
 
     private DepartmentDto createDepartmentDto(int id, String name, BigDecimal avgSalary) {
