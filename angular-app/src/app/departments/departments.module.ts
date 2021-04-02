5b27b73fc5b2986fd7a2a27764f98d2f000d857c
Siarhei Berdachuk <berdachuk@gmail.com>
2021-04-02 20:58:10 +0300

    sample form

--- angular-app/src/app/departments/departments.module.ts
+++ angular-app/src/app/departments/departments.module.ts
@@ -5,13 +5,15 @@ import {DepartmentsRoutingModule} from './departments-routing.module';
 import {DepartmentListComponent} from './components/department-list/department-list.component';
 import {DepartmentFormComponent} from './components/department-form/department-form.component';
 import {DepartmentsComponent} from './departments.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [DepartmentListComponent, DepartmentFormComponent, DepartmentsComponent],
   imports: [
     CommonModule,
-    DepartmentsRoutingModule
+    DepartmentsRoutingModule,
+    FormsModule
   ]
 })
 export class DepartmentsModule { }
--- angular-app/src/app/services/department.service.ts
+++ angular-app/src/app/services/department.service.ts
@@ -3,11 +3,13 @@ import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DepartmentDto} from "../model/department-dto";
 import {catchError, map, tap} from 'rxjs/operators';
+import {Department} from "../model/department";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
+
   private basePath = 'http://localhost:8088';
 
   constructor(protected httpClient: HttpClient) {
@@ -20,6 +22,41 @@ export class DepartmentService {
         tap(response => console.log(JSON.stringify(response))),
         catchError(this.handleError)
       );
+  }
+
+  public findDepartmentById(id: number | string): Observable<Department> {
+    return this.httpClient.get<Department>(`${this.basePath}/departments/${encodeURIComponent(String(id))}`);
+  }
+
+  public createDepartment(department: Department): Observable<number> {
+    if (department === null || department === undefined) {
+      throw new Error(
+        'Required parameter department was null or undefined when calling createDepartment.'
+      );
+    }
+
+    // let headers = new HttpHeaders();
+    // headers.append('Accept', 'application/json');
+    // headers.append('Content-Type', 'application/json');
+    return this.httpClient.post<number>(`${this.basePath}/departments`, department);
+  }
+
+  public updateDepartment(department: Department): Observable<number> {
+    if (department === null || department === undefined) {
+      throw new Error(
+        'Required parameter department was null or undefined when calling updateDepartment.'
+      );
+    }
+    return this.httpClient.put<number>(`${this.basePath}/departments`, department);
+  }
+
+  public deleteDepartment(id: number): Observable<number> {
+    if (id === null || id === undefined) {
+      throw new Error(
+        'Required parameter id was null or undefined when calling deleteDepartment.'
+      );
+    }
+    return this.httpClient.delete<number>(`${this.basePath}/departments/${encodeURIComponent(String(id))}`);
   }
 
   private handleError(err: HttpErrorResponse): Observable<never> {
@@ -37,5 +74,5 @@ export class DepartmentService {
     console.error(errorMessage);
     return Observable.throw(errorMessage);
   }
-
 }
+
