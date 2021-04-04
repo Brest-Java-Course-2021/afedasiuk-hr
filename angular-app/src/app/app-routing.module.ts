4e6381d1b0eaf200ee9233aca724a1ff57d67dc3
Siarhei Berdachuk <berdachuk@gmail.com>
2021-04-04 10:45:00 +0300

    navigation fix

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -4,17 +4,17 @@ import {PageNotFoundComponent} from "./components";
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/departments',
+    pathMatch: 'full'
+  },
+  {
     path: 'departments',
     loadChildren: () => import('./departments/departments.module').then(m => m.DepartmentsModule)
   },
   {
     path: 'employees',
     loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule)
-  },
-  {
-    path: '',
-    redirectTo: '/departments',
-    pathMatch: 'full'
   },
   {
     path: '**',
